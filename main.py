419301046463d499d0f48cb1d313518509522816
Anthony <leisx012@umn.edu>
2018-08-03 18:51:37 -0500

    Toggle buttons complete

--- main.py
+++ main.py
@@ -3,25 +3,22 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.togglebutton import ToggleButton
 from kivy.graphics import Color, Ellipse, Line
+from kivy.uix.slider import Slider
+
 from random import random
 
 
 class PaintWidget(Widget):
-    def setup(self):
-        self.color = (random(), random(), random())
+    def __init__(self):
+        super().__init__()
         self.radius = 30.
-
-    def setColor(self, color):
-        self.color = color
 
     def on_touch_down(self, touch):
         with self.canvas:
-            # print(self.parent.getColor())
-            self.setColor(self.parent.getColor())
-            Color(self.color[0], self.color[1], self.color[2])
+            Color(*self.parent.getLineColor())
             d = 30.
             # Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-            touch.ud['line'] = Line(points=(touch.x, touch.y))
+            touch.ud['line'] = Line(points=(touch.x, touch.y), width=self.parent.getLineWidth())
 
     def on_touch_move(self, touch):
         with self.canvas:
@@ -29,23 +26,23 @@ class PaintWidget(Widget):
 
 class MyToggleButton(ToggleButton):
     def on_press(self):
-        self.parent.setColor(self.color)
+        self.parent.setLineColor(self.color)
 
     def setup(self, color):
         self.colorData = color
 
-class MyWidget(Widget):
+class WrapperWidget(Widget):
     def __init__(self):
         super().__init__()
         self.painter = PaintWidget()
-        self.painter.setup()
-        clearbtn = Button(text='Clear')
-        self.currentColor = (1,1,1,1)
-        self.colors = [self.currentColor]
+        self.currentLineColor = (1,1,1,1)
+        self.lineWidth = 5.0
+        self.colors = [self.currentLineColor]
         self.addColor((.1,1,1,1))
         self.addColor((1,.1,1,1))
         self.addColor((1,1,.1,1))
         self.addColor((1,.1,.1,1))
+        clearbtn = Button(text='Clear')
         clearbtn.bind(on_release=self.clear_canvas)
         self.add_widget(self.painter)
         self.add_widget(clearbtn)
@@ -59,15 +56,21 @@ class MyWidget(Widget):
     def clear_canvas(self, obj):
         self.painter.canvas.clear()
 
-    def getColor(self):
-        return self.currentColor
+    def getLineColor(self):
+        return self.currentLineColor
 
-    def setColor(self, color):
-        self.currentColor = (color[0], color[1], color[2], color[3])
+    def setLineColor(self, color):
+        self.currentLineColor = (color[0], color[1], color[2])
+
+    def getLineWidth(self):
+        return self.lineWidth
+
+    def setLineWidth(self, width):
+        self.lineWidth = width
 
 class PaintApp(App):
     def build(self):
-        parent = MyWidget()
+        parent = WrapperWidget()
 
         return parent
 
